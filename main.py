985dd6b72cad75c1e919236177cbc48742f7d109
zal <zalea473@gmail.com>
2026-07-14 15:22:29 +0700

    refactor: Organize webcam setup and color configuration sections for better readability

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import cv2 as cv
 import numpy as np
 import math
 
+# --- WEBCAM SETUP ---
 capture = cv.VideoCapture(0, cv.CAP_DSHOW) # 0 means the default webcam, if you have multiple webcams, you can change the number to 1, 2, etc.
-# CAP_DSHOW is a flag that tells OpenCV to use the DirectShow backend for video capture, which is a Windows-specific API for capturing video from webcams and other video devices. It can help improve compatibility and performance when using certain webcams on Windows systems.
-
 capture.set(3, 640) # 3 is a code number for the width of the webcam, 640 is the width in pixels
 capture.set(4, 480) # 4 is a code number for the height of the webcam, 480 is the height in pixels
 capture.set(10, 150) # 10 is a code number for the brightness of the webcam, 150 is the brightness value (0-255)
 
-myColors = [[24, 28, 187, 57, 255, 255], # yellow -> [h_min, s_min, v_min, h_max, s_max, v_max]
-            [175, 67, 142, 179, 255, 255]] # orange
+# --- COLOR CONFIGURATION ---
+myColors = [[24, 28, 187, 57, 255, 255], # -> [h_min, s_min, v_min, h_max, s_max, v_max]
+            [175, 67, 142, 179, 255, 255]]
 
-myColorValues = [[0, 255, 222],
-                 [0, 68, 255]] # in BGR format, these are the colors that will be used to draw the contours of the detected colors
+myColorValues = [[0, 255, 222],  # -> in BGR format, these are the colors that will be used to draw the contours of the detected colors
+                 [0, 68, 255]] 
 
 def getContours(mask):
     x, y, width, height = 0, 0, 0, 0
@@ -49,6 +49,7 @@ while True:
     # get the current points of the detected colors
     currentPoints = findColor(webcam, myColors) 
     
+    # DYNAMIC LOOP FOR EVERY COLOR
     for i in range(len(currentPoints)):
         x, y = currentPoints[i][0], currentPoints[i][1]
         px, py = prevPoints[i][0], prevPoints[i][1]
